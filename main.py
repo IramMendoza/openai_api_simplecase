00b3cb7fcc60cf511619cb1bc730c9b29e9f3935
Iram Mendoza <123773810+IramAnt@users.noreply.github.com>
2023-08-02 16:54:49 -0600

    fixed venv

--- main.py
+++ main.py
@@ -6,9 +6,19 @@ dotenv.load_dotenv()
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
-messages = [{ 'role' : 'system', 'content' : 'Eres un desarrollador senior'},
-           { 'role' : 'user', 'content' : '¿Es cierto que python es un lenguaje relativamente lento?'}]
+role = '' # How you want to answer the question. example: 'I am a doctor'
+
+prompt = '' # The question you want to ask. example: 'How many years the elephant lives?'
+
+messages = [{ 'role' : 'system', 'content' : role },
+           { 'role' : 'user', 'content' : prompt}]
+
+# Change the model to your desired model. example: 'davinci'
+
+# max_tokens = 200 # The maximum number of tokens to be generated. example: 100
 
 response = openai.ChatCompletion.create(model='gpt-3.5-turbo', messages=messages, max_tokens=200)
 
-print (str(response.choices[0].message.content), f'  Tokens utilizados : {str(response.usage.total_tokens)}')
+#  It prints the generated answer and the number of tokens used.
+
+print (str(response.choices[0].message.content), f'  Used Tokens : {str(response.usage.total_tokens)}')
